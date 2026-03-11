57ec519f7fbea440914611d7a281f2302867ee2a
navaneethchowdary38-lang <navaneethchowdary38@gmail.com>
2026-03-11 16:07:28 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+
+from config import load_llm
+from rag.pdf_processor import extract_text, split_text
+from rag.vector_store import build_vector_db
+from rag.qa_chain import ask_question
+from image.image_qa import ask_image_question
+
+st.set_page_config(page_title="SlideSense AI")
+
+st.title("🧠 SlideSense")
+
+mode = st.radio("Mode", ["PDF", "Image"])
+
+llm = load_llm()
+
+if mode == "PDF":
+
+    pdf = st.file_uploader("Upload PDF")
+
+    if pdf:
+
+        text = extract_text(pdf)
+
+        chunks = split_text(text)
+
+        vector_db = build_vector_db(chunks)
+
+        question = st.text_input("Ask question")
+
+        if question:
+
+            answer = ask_question(llm, vector_db, question)
+
+            st.write(answer)
+
+else:
+
+    image = st.file_uploader("Upload Image")
+
+    question = st.text_input("Ask about the image")
+
+    if image and question:
+
+        answer = ask_image_question(llm, image, question)
+
+        st.write(answer)
